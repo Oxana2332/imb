df51505cb22830067046b4d246963ec803c40014
Oxana2332 <far_north@ro.ru>
2025-01-14 23:15:02 +0300

    первоначальная версия калькулятора ИМТ

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+let height = document.querySelector('.height');
+let weight = document.querySelector('.weight');
+let imbResult = document.querySelector('.result');
+let button = document.querySelector('.button');
+
+function imb(weight, height) {
+	height /= 100;
+	result = (weight / height ** 2).toFixed(1);
+	if (result <= 16) {
+		imbResult.textContent = `${result} — Выраженный дефицит массы тела`;
+	} else if (result <= 18.5) {
+		imbResult.textContent = `${result} — Недостаточная (дефицит) масса тела`;
+	} else if (result <= 25) {
+		imbResult.textContent = `${result} — Норма`;
+	} else if (result <= 30) {
+		imbResult.textContent = `${result} — Избыточная масса тела (предожирение)`;
+	} else if (result <= 35) {
+		imbResult.textContent = `${result} — Ожирение первой степени`;
+	} else if (result <= 40) {
+		imbResult.textContent = `${result} — Ожирение второй степени`;
+	} else if (result >= 40) {
+		imbResult.textContent = `${result} — Ожирение третьей степени (морбидное)`;
+	} else {
+		imbResult.textContent = `Проверьте правильность введённых данных`;
+	}
+}
+
+button.addEventListener('click', () => imb(weight.value, height.value));
